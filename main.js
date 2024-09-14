f4c1c2a1c78bdf5567971e676f6836377f300c4d
Vlad-Cusnir <vlad.cusnir@gmail.com>
2024-09-14 13:21:23 +0200

    ok

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Hello World");
+alert("i am a happy programer");
+
+let firstNr = prompt("Input the first numebr");
+firstNr = Number(firstNr);
+
+let secoundNr = prompt("Input the secound numebr");
+secoundNr = Number(secoundNr);
+
+let sum = firstNr + secoundNr;
+
+alert(sum);

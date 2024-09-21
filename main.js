2452b91879e6f36e5a6ecdca9837a69941587162
Vlad-Cusnir <vlad.cusnir@gmail.com>
2024-09-21 08:54:27 +0200

    change js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 alert("Hello World");
 alert("i am a happy programer");
+// DATA input Section 
 
 let firstNr = prompt("Input the first numebr");
 firstNr = Number(firstNr);
@@ -7,6 +8,8 @@ firstNr = Number(firstNr);
 let secoundNr = prompt("Input the secound numebr");
 secoundNr = Number(secoundNr);
 
+//Processing section
 let sum = firstNr + secoundNr;
 
+// Data output section
 alert(sum);
